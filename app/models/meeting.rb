0387606aea44ef86683c1a7685a4de100f566bf2
Nikitin Egor <negor.dev@gmail.com>
2017-05-15 12:06:55 +0300

    init

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,8 @@
 class Meeting < ApplicationRecord
+	has_many :comments, dependent: :destroy
 	validates :title, :description, :image_url, presence: true
 	validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'должен указывать на изображение формата GIF, JPG или PNG.'
 	}
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513160502) do
+ActiveRecord::Schema.define(version: 20170515083328) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "meeting_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meeting_id"], name: "index_comments_on_meeting_id"
+  end
 
   create_table "meetings", force: :cascade do |t|
     t.string   "title"
