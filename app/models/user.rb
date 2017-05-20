f281c7a777198e1db7e2aa1f6188f7cfd84823d4
Nikitin Egor <negor.dev@gmail.com>
2017-05-20 12:14:58 +0300

    S3 Downloads

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,20 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :meetings
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  has_attached_file :avatar, 
+    styles: { medium: "300x300#", thumb: "100x100#" },
+    :convert_options => {
+    :thumb => "-quality 75 -strip" },
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => ENV['AWS_REGION']
+    },
+    :path => ":filename.:extension",
+    :default_url => "default_img.png"
 
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_secure_password
 end
